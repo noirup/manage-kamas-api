824f20da0bc1b8d7a3316afce7c63d9027221757
Raphaël SEEGER <raphael.seeger@student.vinci.be>
2021-01-31 18:25:35 +0100

    initial commit

---
+++ src/test/java/com/acme/ManageKamasApi/ManageKamasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.ManageKamasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageKamasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
